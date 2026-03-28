e36408c16d8c80c468eb1f9bc107be357ae47a46
Dylan <dylan.moulin9272@gmail.com>
2026-03-28 12:57:49 +0100

    udpate views.search.search-bar : 'Search' replaced with 'Rechercher'

--- resources/views/component/search-bar.blade.php
+++ resources/views/component/search-bar.blade.php
@@ -1,7 +1,7 @@
 <div class="tm-search-wrapper">
     <form class="d-flex tm-search-form" action="/catalog?q=">
         <input class="form-control tm-search-input" type="search"
-               placeholder="Search" aria-label="Search" id="search-bar" name="q" autocomplete="off">
+               placeholder="Rechercher" aria-label="Search" id="search-bar" name="q" autocomplete="off">
         <button class="btn btn-outline-success tm-search-btn" type="submit">
             <i class="fas fa-search"></i>
         </button>
